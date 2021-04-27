f1199b54d629ce3252368b1c316557f49804c6d4
peruibeloko <cmendonca32@gmail.com>
2021-04-26 23:34:49 -0300

    fix logging

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const router = require('express').Router();
+const router = express.Router();
 const posts = require('./posts');
 
 const app = express();
 
-router.all('*', (req, res, next) => {
+router.use(function (req, res, next) {
   console.log(
-    `Received ${req.method} ${req.path}${req.params ? ' with parameters ' : ''}`,
+    `[${new Date().toLocaleString('pt-BR')}] Received ${req.method} ${req.path}${req.params ? ' with parameters' : ''}`,
     req.params
   );
   next();
 });
 
+app.use(router);
 app.use('/post', posts);
 
 module.exports = app;
