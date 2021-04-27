c8f4091865bd87261b8aefa259faaeed74b9e3a6
peruibeloko <cmendonca32@gmail.com>
2021-04-26 23:10:36 -0300

    add healthcheck

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(cors());
 
 app.use(routes);
 
+app.use('/healthcheck', (req, res) => res.send('OK'));
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
