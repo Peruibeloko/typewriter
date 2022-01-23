562d0760d5f7b8e6a6fa1a509300d743168103ff
peruibeloko <cmendonca32@gmail.com>
2022-01-23 20:11:04 -0300

    patch last few security holes

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,34 +7,45 @@ import User from '../models/userModel.js';
 export const signup = async (req, res) => {
   const { email } = req.body;
 
+  if (!email) return res.status(400).send('Missing user email');
+
   const queryResult = await Allowlist.findById(email).exec();
 
   if (!queryResult) return res.status(403).send('User not in allowlist');
+  if (queryResult.isRegistered) return res.status(409).send('User already registered');
 
-  const secret = authenticator.generate();
+  await Allowlist.findByIdAndUpdate(email, { isRegistered: true }).exec();
 
-  User.create({ _id: email, secret, timestamp: Date.now() }, (err, doc) => {
-    res.send(err || secret);
-  });
+  const secret = authenticator.generateSecret();
+
+  const user = new User({ _id: email, secret, timestamp: Date.now() });
+  try {
+    await user.save();
+    res.send(secret);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 export const login = async (req, res) => {
   const user = await User.findById(req.body.email).exec();
 
-  if (!user) return res.status(404).send(`No user for email ${user.email} was found`);
+  if (!user) return res.status(404).send(`No user for email ${user._id} was found`);
 
-  const isTokenValid = authenticator.verify({
-    secret: user.secret,
-    token: req.body.token
-  });
+  const isTokenValid = authenticator.check(`${req.body.token}`, user.secret);
 
   if (!isTokenValid) return res.status(403).send('Invalid OTP');
 
-  const hash = crypto.createHash('sha256');
-  hash.update(`${user.secret}${user.timestamp}`);
-  const signingSecret = hash.digest('hex');
+  const signingSecret = crypto
+    .createHash('sha256')
+    .update(`${user.secret}${user.timestamp}`)
+    .digest('hex');
 
-  const jwt = jsonwebtoken.sign({ email: user.email }, signingSecret, { expiresIn: '12h' });
+  const jwt = jsonwebtoken.sign({}, signingSecret, {
+    audience: `${user._id}`,
+    expiresIn: '12h'
+  });
+
   res.send(jwt);
 };
 
@@ -43,13 +54,18 @@ export const checkAuth = async (req, res, next) => {
 
   if (!authHeader) return res.status(403).send('Missing authentication header');
 
-  const decodedJWT = JSON.parse(jsonwebtoken.decode(authHeader));
+  const decodedJWT = jsonwebtoken.decode(authHeader);
 
-  const user = await User.findById(decodedJWT.email, 'secret timestamp').exec();
+  if (!decodedJWT) return res.status(403).send('Malformed authentication header');
 
-  const hash = crypto.createHash('sha256');
-  hash.update(`${user.secret}${user.timestamp}`);
-  const signingSecret = hash.digest('hex');
+  const user = await User.findById(decodedJWT.aud, 'secret timestamp').exec();
+
+  if (!user) return res.status(403).send('Unknown user');
+
+  const signingSecret = crypto
+    .createHash('sha256')
+    .update(`${user.secret}${user.timestamp}`)
+    .digest('hex');
 
   try {
     jsonwebtoken.verify(authHeader, signingSecret);
--- models/allowlistModel.js
+++ models/allowlistModel.js
@@ -6,6 +6,10 @@ const allowlistSchema = new Schema({
   _id: {
     type: String,
     required: 'User email'
+  },
+  isRegistered: {
+    type: Boolean,
+    default: false
   }
 });
 
