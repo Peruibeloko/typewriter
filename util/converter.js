2b81c3b6a2706e7062edc8b0491a486f6ff1df92
peruibeloko <cmendonca32@gmail.com>
2022-05-22 20:45:46 -0300

    fuck this, imma fix it in the frontend

--- util/converter.js
+++ util/converter.js
@@ -1,11 +1,3 @@
 import { marked } from 'marked';
 
-marked.use({
-  renderer: {
-    blockquote(quote) {
-      return `<blockquote>${quote}</blockquote>`;
-    }
-  }
-});
-
 export const printMarkdownToHTML = markdown => marked.parse(markdown);
