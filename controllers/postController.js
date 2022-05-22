a58f8a2812a13c4988995b987ac731f1871e8e00
peruibeloko <cmendonca32@gmail.com>
2022-05-22 20:22:31 -0300

    post creation returns id of created post

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,8 +20,8 @@ export const getPaginatedPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   const newPost = new Post({ ...req.body });
   try {
-    await newPost.save();
-    res.sendStatus(201);
+    const { _id } = await newPost.save();
+    res.status(201).json(_id);
   } catch (err) {
     res.status(500).send(err);
   }
